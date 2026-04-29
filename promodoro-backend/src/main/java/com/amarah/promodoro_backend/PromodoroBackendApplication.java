68b630e998f45cd2dfff1ddf87dac3765161c562
mekill404 <hei.mahery.55@gmail.com>
2026-04-29 23:21:20 +0000

    initialisation du backend

---
+++ promodoro-backend/src/main/java/com/amarah/promodoro_backend/PromodoroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.amarah.promodoro_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromodoroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromodoroBackendApplication.class, args);
+	}
+
+}
---
+++ promodoro-backend/src/test/java/com/amarah/promodoro_backend/PromodoroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amarah.promodoro_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromodoroBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
